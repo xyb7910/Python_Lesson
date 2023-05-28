1d18475644703a74051684b87165ad88c2e03d6e
xyb7910 <1502709024@qq.com>
2023-05-28 15:58:27 +0800

    ATM的简单实现

--- Day04/Modules/ATM/AtmDemo.py
+++ Day04/Modules/ATM/AtmDemo.py
@@ -4,20 +4,22 @@ Date: 2023-5-28
 content: atm_module_demo project
 """
 
-import atm
+from atm import *
 
-atm.select_fun()
+select_fun()
 
-# 调用函数
-# 搭函数框架
-print("密码正确，登陆成功！")
-# 显示"选择功能"界面
-atm.select_fun()
+num = int(input("请输入你所要执行的业务"))
 
-print("查询余额完毕")
-# 显示"选择功能"界面
-atm.select_fun()
-
-print("取了2000元钱")
-# 显示"选择功能"界面
-atm.select_fun()
+if num == 1:
+    card_num = int(input("请输入你的卡号"))
+    find(card_num)
+elif num == 2:
+    card_num = int(input("请输入你的卡号"))
+    num = int(input("请输入你所要存入的现金"))
+    add(card_num, num)
+elif num == 3:
+    card_num = int(input("请输入你的卡号"))
+    num = int(input("请输入你所要取走的现金"))
+    withdrawal(card_num, num)
+else:
+    print("欢迎下次使用，祝你一路顺风")
--- Day04/Modules/ATM/atm.py
+++ Day04/Modules/ATM/atm.py
@@ -1,6 +1,26 @@
 def select_fun():
     print("-----请选择功能-----")
-    print("查询余额")
-    print("存款")
-    print("取款")
+    print("1查询余额")
+    print("2存款")
+    print("3取款")
+    print("4退出系统")
     print("-------------------")
+
+
+card = {1: 102, 2: 901, 3: 876, 4: 897, 5: 111, 6: 435}
+
+
+def find(card_num):
+    print(f"你账户的余额为{card[card_num]}")
+
+
+def add(card_num, num):
+    print(f"你账户的余额为{card[card_num]}")
+    card[card_num] += num
+    print(f"目前你账户的余额为{card[card_num]}")
+
+
+def withdrawal(card_num, num):
+    print(f"你账户的余额为{card[card_num]}")
+    card[card_num] -= num
+    print(f"目前你账户的余额为{card[card_num]}")
